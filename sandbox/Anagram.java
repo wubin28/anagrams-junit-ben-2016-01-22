e109e8772408b22842a96c7a45adc32c2b033d25
vulture_6227002B54 <vulture@cyber-dojo.org>
2016-01-19 11:36:44 +0000

    15

--- sandbox/Anagram.java
+++ sandbox/Anagram.java
@@ -5,7 +5,7 @@ public class Anagram {
 
     public List<String> generate(String string) {
         if (string.length() == 2) {
-            return Arrays.asList("ab", "ba");
+            return Arrays.asList(string, "ba");
         }
         return Arrays.asList(string);
     }
