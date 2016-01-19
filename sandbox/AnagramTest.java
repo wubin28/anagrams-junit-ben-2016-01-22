8d2136d3f969efa98f5a1236253e4a790c8366a2
vulture_6227002B54 <vulture@cyber-dojo.org>
2016-01-19 12:34:51 +0000

    29

--- sandbox/AnagramTest.java
+++ sandbox/AnagramTest.java
@@ -13,26 +13,12 @@ public class AnagramTest {
 
     @Test
     public void the_number_of_the_anagrams_of_two_letter_word_should_be_2() {
-        Anagram anagram = new Anagram();
-
-        List<String> result = anagram.generate("ab");
-
-        assertTrue(result.contains("ab"));
-        assertTrue(result.contains("ba"));
+        assertThat(Anagrams.of("ab"), is(Arrays.asList("ab", "ba")));
     }
 
     @Test
     public void the_number_of_the_anagrams_of_three_letter_word_should_be_6() {
-        Anagram anagram = new Anagram();
-
-        List<String> result = anagram.generate("abc");
-
-        assertTrue(result.contains("abc"));
-        assertTrue(result.contains("acb"));
-        assertTrue(result.contains("bac"));
-        assertTrue(result.contains("bca"));
-        assertTrue(result.contains("cab"));
-        assertTrue(result.contains("cba"));
+        assertThat(Anagrams.of("abc"), is(Arrays.asList("abc", "acb", "bac", "bca", "cab", "cba"));
     }
 
     // TODO: the_number_of_the_anagrams_of_four_letter_word_should_be_24
