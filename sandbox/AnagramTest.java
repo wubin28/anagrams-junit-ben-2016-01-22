18f34e0c829f17090f6cfe89455d2bf9977fa778
vulture_6227002B54 <vulture@cyber-dojo.org>
2016-01-19 11:09:24 +0000

    11

--- sandbox/AnagramTest.java
+++ sandbox/AnagramTest.java
@@ -9,4 +9,8 @@ public class AnagramTest {
 
         assertTrue(anagram.generate("a").contains("a"));
     }
+
+    // TODO: the_number_of_the_anagrams_of_two_letter_word_should_be_2
+    // TODO: the_number_of_the_anagrams_of_three_letter_word_should_be_6
+    // TODO: the_number_of_the_anagrams_of_four_letter_word_should_be_24
 }
