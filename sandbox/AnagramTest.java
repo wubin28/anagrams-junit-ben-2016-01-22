062930313360c261c8d451e53e557e95475be8e6
vulture_6227002B54 <vulture@cyber-dojo.org>
2016-01-19 10:58:40 +0000

    4

--- sandbox/AnagramTest.java
+++ sandbox/AnagramTest.java
@@ -4,9 +4,7 @@ import static org.junit.Assert.*;
 public class AnagramTest {
 
     @Test
-    public void life_the_universe_and_everything() {
-        int expected = 42;
-        int actual = Anagram.answer();
-        assertEquals(expected, actual);
+    public void the_anagram_of_a_one_letter_word_should_be_itself() {
+        assertTrue(anagram.generate("a").contains("a"));
     }
 }
