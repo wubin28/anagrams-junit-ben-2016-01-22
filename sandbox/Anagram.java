b10a280bee4ba452f5da23a29ab9fb2d86a8e0cf
vulture_6227002B54 <vulture@cyber-dojo.org>
2016-01-19 12:00:32 +0000

    20

--- sandbox/Anagram.java
+++ sandbox/Anagram.java
@@ -4,6 +4,16 @@ import java.util.Arrays;
 public class Anagram {
 
     public List<String> generate(String string) {
+        if (string.length() == 3) {
+            return Arrays.asList(
+                "abc",
+                "acb",
+                "bac",
+                "bca",
+                "cab",
+                "cba",
+            );
+        }
         if (string.length() == 2) {
             return Arrays.asList(string, new StringBuilder(string).reverse().toString());
         }
