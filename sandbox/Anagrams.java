456d8af67a16bfebb731045161ce596ba18cb747
vulture_6227002B54 <vulture@cyber-dojo.org>
2016-01-19 14:17:54 +0000

    55

--- sandbox/Anagrams.java
+++ sandbox/Anagrams.java
@@ -10,14 +10,14 @@ public class Anagrams {
         if (s.length() == 2) {
             return Arrays.asList(s, s.substring(1, 2) + s.substring(0, 1));
         }
-        return Arrays.asList(
-            s.substring(0, 1) + Anagrams.of(dropChar(s, 0)).get(0),
-            s.substring(0, 1) + Anagrams.of(dropChar(s, 0)).get(1),
-            s.substring(1, 2) + Anagrams.of(dropChar(s, 1)).get(0),
-            s.substring(1, 2) + Anagrams.of(dropChar(s, 1)).get(1),
-            s.substring(2, 3) + Anagrams.of(dropChar(s, 2)).get(0),
-            s.substring(2, 3) + Anagrams.of(dropChar(s, 2)).get(1)
-        );
+        List<String> anagrams = new ArrayList<String>();
+        anagrams.add(s.substring(0, 1) + Anagrams.of(dropChar(s, 0)).get(0));
+        anagrams.add(s.substring(0, 1) + Anagrams.of(dropChar(s, 0)).get(1));
+        anagrams.add(s.substring(1, 2) + Anagrams.of(dropChar(s, 1)).get(0));
+        anagrams.add(s.substring(1, 2) + Anagrams.of(dropChar(s, 1)).get(1));
+        anagrams.add(s.substring(2, 3) + Anagrams.of(dropChar(s, 2)).get(0));
+        anagrams.add(s.substring(2, 3) + Anagrams.of(dropChar(s, 2)).get(1));
+        return anagrams;
     }
 
     private static String dropChar(String s, int index) {
