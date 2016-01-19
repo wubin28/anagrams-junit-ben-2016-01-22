8bf3ebeb8c0af6c9e3f5f914c9b589d522a21929
vulture_6227002B54 <vulture@cyber-dojo.org>
2016-01-19 11:06:00 +0000

    10

--- sandbox/Anagram.java
+++ sandbox/Anagram.java
@@ -4,6 +4,6 @@ import java.util.Arrays;
 public class Anagram {
 
     public List<String> generate(String string) {
-        return Arrays.asList("a");
+        return Arrays.asList(string);
     }
 }
