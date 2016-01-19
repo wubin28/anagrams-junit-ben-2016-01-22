58c0bf16196ba2b7e905e0ffe13f01206d838917
vulture_6227002B54 <vulture@cyber-dojo.org>
2016-01-19 11:57:21 +0000

    17

--- sandbox/AnagramTest.java
+++ sandbox/AnagramTest.java
@@ -7,17 +7,34 @@ public class AnagramTest {
     public void the_anagram_of_a_one_letter_word_should_be_itself() {
         Anagram anagram = new Anagram();
 
-        assertTrue(anagram.generate("a").contains("a"));
+        String result = anagram.generate("a");
+
+        assertTrue(result.contains("a"));
     }
 
     @Test
     public void the_number_of_the_anagrams_of_two_letter_word_should_be_2() {
         Anagram anagram = new Anagram();
 
-        assertTrue(anagram.generate("ab").contains("ab"));
-        assertTrue(anagram.generate("ab").contains("ba"));
+        String result = anagram.generate("ab");
+
+        assertTrue(result.contains("ab"));
+        assertTrue(result.contains("ba"));
     }
 
-    // TODO: the_number_of_the_anagrams_of_three_letter_word_should_be_6
+    @Test
+    public void the_number_of_the_anagrams_of_three_letter_word_should_be_6() {
+        Anagram anagram = new Anagram();
+
+        String result = anagram.generate("abc");
+
+        assertTrue(result.contains("abc"));
+        assertTrue(result.contains("acb"));
+        assertTrue(result.contains("bac"));
+        assertTrue(result.contains("bca"));
+        assertTrue(result.contains("cab"));
+        assertTrue(result.contains("cba"));
+    }
+
     // TODO: the_number_of_the_anagrams_of_four_letter_word_should_be_24
 }
